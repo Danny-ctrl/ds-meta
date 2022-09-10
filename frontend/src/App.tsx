42d6b0ee81387570efa460f75cfbc8e28c23998a
Daniele Freitas <contatodanny@hotmail.com>
2022-09-09 21:48:03 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-
+import NotificationButton from './assets/components/NotificationButton'
 
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
-
-  
 }
 
 export default App
