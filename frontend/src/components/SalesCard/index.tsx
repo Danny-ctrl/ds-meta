86e9d358cc2a6bae3bebec3b952c3086554ee9af
Daniele Freitas <contatodanny@hotmail.com>
2022-09-11 20:36:26 -0300

    Sale listing

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -60,13 +60,15 @@ function SalesCard() {
           <tbody>
             {sales.map(sale => {
               return (
-                <tr>
-                  <td className="show992">#341</td>
-                  <td className="show576">08/07/2022</td>
-                  <td>Anakin</td>
-                  <td className="show992">15</td>
-                  <td className="show992">11</td>
-                  <td>R$55300.00</td>
+                <tr key={sale.id}>
+                  <td className="show992">{sale.id}</td>
+                  <td className="show576">
+                    {new Date(sale.date).toLocaleDateString()}
+                  </td>
+                  <td>{sale.sellerName}</td>
+                  <td className="show992">{sale.visited}</td>
+                  <td className="show992">{sale.deals}</td>
+                  <td>{sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
                       <NotificationButton />
